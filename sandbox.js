f5230b2302557e40bafaeab7da3f26d5c45c31c9
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-28 12:48:57 +0530

    a,o

--- sandbox.js
+++ sandbox.js
@@ -7,17 +7,71 @@ console.log(inputs);
 
 inputs.forEach(input => {
     console.log(input);
-})*/
-var character = 'mario';
-var age = 21;
-var isBlackBelt = false;
+})
+
+let character = 'mario';
+let age = 21;
+let isBlackBelt =false;
+
 // character = 20; is not possible
 character = 'nikks';
+
 //age ='yoshi'; is not possible
 age = 29;
+
 //isBlackBelt = 'yes'; is not possible
 isBlackBelt = true;
-var circ = function (diameter) {
+
+const circ = (diameter: number) => {
     return diameter * Math.PI;
-};
+}
+
 console.log(circ(6.8));
+
+//arrays
+let names = ['uri', 'mona', 'seenu'];
+names.push('reena');
+//names.push(2); is not possible
+//names[0] = 3; is not possible
+//name = 'juhi' is not possible cannot change array type.
+
+let numbers = [11, 23, 45]
+numbers.push(47);
+//numbers.push('reku'); is not possible
+//numbers[2] = 'ruune'; is not possible
+
+let mixed = ['tunee', 2, true, 56, 'eelu'];
+mixed.push('teena');
+mixed.push(34);
+mixed.push(false);
+
+//objects
+let ninja = {
+    name: 'mari',
+    belt: 'blue',
+    age: 20
+};
+
+ninja.age = 29;
+ninja.name = 'ryu';
+//ninja.age = 'ryu'; is not possible
+//ninja.skills = ['fight', 'boxing']; is not possible
+ninja = {
+    name: 'nnari',
+    belt: 'black',
+    age: 24
+};
+
+
+/*  is not possible
+ninja = {
+    name: 'ari',
+    belt: 'black',
+    //age: 89  all must be there name,belt,age.
+};
+ninja = {
+    name: 'ari',
+    belt: 'black',
+    age: 89
+    skills: ['fight', 'skill'] nothing can be added other than name,belt,age.
+}; */
